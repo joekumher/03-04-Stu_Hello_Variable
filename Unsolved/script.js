653d1e81e436fafaed55647a29cd7f51958ce52a
Joseph Kumher <joekumher@gmail.com>
2022-08-15 15:19:13 -0400

    defined variables for Ricky and added console logs

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -6,3 +6,11 @@ var funFact = 'I was born on Leap Day'
 console.log('My name is ' + personName + '.')
 console.log('I have ' + pets + ' pet ')
 console.log('Fun Fact: ' + funFact);
+
+personName = 'Ricky';
+pets = 174;
+funFact = 'I am the only modern case of Bubonic Plague'
+
+console.log('My name is ' + personName + '.')
+console.log('I have ' + pets + ' pet ')
+console.log('Fun Fact: ' + funFact);
