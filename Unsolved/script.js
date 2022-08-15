6d63a808489751a4e7ef15d8e2d05b027a0badb0
Joseph Kumher <joekumher@gmail.com>
2022-08-15 15:15:40 -0400

    defined a few variables such as personname pets and fun facts

---
+++ Unsolved/script.js
@@ -0,0 +1,8 @@
+// Write Your JavaScript Code Here
+var personName = 'Joe'
+var pets = 1;
+var funFact = 'I was born on Leap Day'
+
+console.log('My name is ' + personName + '.')
+console.log('I have ' + pets + ' pet ')
+console.log('Fun Fact: ' + funFact);
